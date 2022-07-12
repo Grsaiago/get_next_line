84a1184c711899fd45935c53357a8f2f812dcaef
gsaiago <grsaiago@gmail.com>
2022-07-12 14:52:03 -0300

    This

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gsaiago <gsaiago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 14:49:43 by gsaiago           #+#    #+#             */
-/*   Updated: 2022/07/12 14:30:49 by gsaiago          ###   ########.fr       */
+/*   Updated: 2022/07/12 14:51:54 by gsaiago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
-int		ft_nchr(const char *s);
 char	*ft_strdup(const char *s1);
 size_t	ft_strlen(const char *c);
 void	*ft_calloc(size_t count, size_t size);
-char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin_gnl(char *s1, char *s2);
 char	*ft_staticptr(char *buffer);
 char	*ft_returnptr(char *buffer);
